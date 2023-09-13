9e6ec14551056cdec4b6c9bc18ea03f9180cbdee
Konstantin Slavin <konstantin.slavin@eosda.com>
2023-09-13 18:27:53 -0400

    (refactor) jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,9 @@
 export default {
   displayName: 'coffee-project',
-  testEnvironment: 'jsdom',
-  extensionsToTreatAsEsm: ['.ts'],
+  preset: 'jest-preset-angular',
+  globalSetup: 'jest-preset-angular/global-setup',
+  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/modules/*.*/'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
